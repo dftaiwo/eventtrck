c6c80399032da494aa20b503bf057fadaa8bf30a
Femi Taiwo <dftaiwo@gmail.com>
2014-05-24 15:56:36 +0100

    Trying to debug whitescreen of death

---
+++ template/create_event.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<form method="POST">
+	<p>
+	<label>
+		Name
+	</label>
+	<input type="text" name="event_name" />
+</p>
+<p>
+	<label>
+		Description
+	</label>
+	<textarea name="description"></textarea>
+	</p>
+	<p>
+		<label>
+		Date
+	</label>
+	<input type="text" name="event_date" />
+	</p>
+	<p>
+		<label>
+		Venue
+	</label>
+	<input type="text" name="venue" />
+	</p>
+	<p><input type="submit" value="Save"/>
+	</p>
+</form>
--- template/events_list.php
+++ template/events_list.php
@@ -1,5 +1,5 @@
 <h3>
-	This should be a list of events
+	This should be a LOOOOOOONG list of events
 	
 </h3>
 <?php pr($events) ; ?>
